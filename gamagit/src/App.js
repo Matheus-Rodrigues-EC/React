89c70993bab1cdd42dd8c97e4884c74019966435
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-07-18 16:41:45 -0300

    Update Aplication
    Adicionado os métodos para fazer requisição em React, bem como a adição de axios

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import Axios from 'axios';
+import axios from 'axios';
 
 function App(props) {
+  const [ usuario, setUsuario] = useState('');
+  
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
+
   return (
+    // O atributo onChage captura qualquer alteração no input
     <>
     <label for="userImput">Usuário: </label>
-    <input name="Usuario" placeholder="Usuário" id="userImput" className="usuarioInput"/>
-    <button type="button">Pesquisar</button>
+    <input placeholder="Usuário" className="usuarioInput" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
     
   )
