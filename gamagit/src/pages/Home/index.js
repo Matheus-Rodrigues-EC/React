6e8e3a387e11e072436d85563302dbd1f702cd78
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-07-21 19:02:58 -0300

    Update projeto react
    Adição dos campos de código de renderização de texto e criação de links com react router dom.

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -2,13 +2,37 @@ import React, { useState } from 'react';
 import Axios from 'axios';
 import axios from 'axios';
 import * as S from './styled';
+import { useHistory } from 'react-router-dom'
 
 function App(props) {
+  const history = useHistory();
   const [ usuario, setUsuario] = useState('');
-  
+
   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      history.push('./repositories')
+    });
   }
+
+  document.body.addEventListener('keyup', (event) => {
+    if(event.code == 'Enter'){
+      axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      history.push('./repositories')
+    })
+    }
+  })
 
   return (
     // O atributo onChage captura qualquer alteração no input
--- gamagit/src/pages/Repositories/index.js
+++ gamagit/src/pages/Repositories/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styled';
 
+
 export default function Repositories() {
+    const [ repositories, setRepositoies] = useState([]);
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        console.log(repositoriesName)
+        setRepositoies(repositoriesName);
+        //localStorage.clear();
+    }, []);
+
     return(
         <S.Container>
         <S.Title>Repositórios</S.Title>
         <S.List>
             <c>Repositório</c>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            
+            {repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: {repository} </S.ListItem>
+                    )
+                })}
+
         </S.List>
+        <S.LinkHome to="/"> Voltar </S.LinkHome>
         </S.Container>
     );
 }
