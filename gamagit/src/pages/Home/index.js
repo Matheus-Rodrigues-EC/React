379498b615a7a895b43c77cd916bab7af21a0dd1
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-07-21 20:20:05 -0300

    Update final
    Adição de Renderização condicional, pesquisa com a tecla Enter e tratamento básico de erros

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -7,41 +7,41 @@ import { useHistory } from 'react-router-dom'
 function App(props) {
   const history = useHistory();
   const [ usuario, setUsuario] = useState('');
+  const [ erro, setErro ] = useState(false);
 
-  function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-      const repositories = response.data;
-      const repositoriesName = [];
-      repositories.map((repository) => {
-        repositoriesName.push(repository.name);
-      });
-      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-      history.push('./repositories')
+  function handlePesquisa(){axios.get(`https://api.github.com/users/${usuario}/repos`)
+  .then(response => {
+    const repositories = response.data;
+    const repositoriesName = [];
+    repositories.map((repository) => {
+      repositoriesName.push(repository.name);
     });
+    localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    setErro(false);
+    history.push('./repositories')
+  })
+  .catch(err => {
+    setErro(true);
+  })
   }
+
 
   document.body.addEventListener('keyup', (event) => {
     if(event.code == 'Enter'){
-      axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-      const repositories = response.data;
-      const repositoriesName = [];
-      repositories.map((repository) => {
-        repositoriesName.push(repository.name);
-      });
-      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-      history.push('./repositories')
-    })
+      handlePesquisa();
     }
   })
 
   return (
     // O atributo onChage captura qualquer alteração no input
-    <S.container>
-      <label for="userImput">Usuário: </label>
-      <S.input placeholder="Usuário" className="usuarioInput" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <S.button type="button" onClick={handlePesquisa}>Pesquisar</S.button>
-    </S.container>
-    
+    <S.HomeConteiner>
+      <S.content>
+        <label for="userImput">Usuário: </label>
+        <S.input placeholder="Usuário" className="usuarioInput" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.button type="button" onClick={handlePesquisa}>Pesquisar</S.button>
+      </S.content>
+      { erro ? <S.ErrorMessage>Ocorreu um erro. Tente novamente.</S.ErrorMessage> : '' }
+    </S.HomeConteiner>
   )
 }
 
--- gamagit/src/pages/Home/styled.js
+++ gamagit/src/pages/Home/styled.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
 
-export const container = styled.div`
-    width: 100%;
+export const HomeConteiner = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
+`;
+
+export const content = styled.div`
+    width: 100vw;
     display: flex;
     align-items: center;
     justify-content: center;
-`
+`;
 
 export const input = styled.input`
 border: 1px solid #ddd;
@@ -27,4 +35,23 @@ export const button = styled.button`
     background: #000;
     color: #fff;
     border-radius: 0.25rem 0.25re, 0;
+`;
+
+export const LinkHome = styled(Link)`
+    display: block;
+    width: 4rem;
+    text-align: center;
+    margin: 2rem auto;
+    background-color: #000;
+    padding: .5rem 0;
+    color: #fff;
+    text-decoration: none;
+`;
+
+export const ErrorMessage = styled.span`
+    display: block;
+    font-size: 0.65rem;
+    color: red;
+    font-weigth: 600;
+    margin-top: 1rem;
 `;
